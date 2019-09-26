c699621ce185191fc718426cbfb10810f809ff2a
kosmonavtika <jimmy@vostok.dev>
2019-09-26 20:13:55 +0200

    fix repository

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -2,7 +2,7 @@
 
 ENTERPRISE_REPO_LIST="/etc/apt/sources.list.d/pve-enterprise.list"
 FREE_REPO_LIST="/etc/apt/sources.list.d/pve-no-subscription.list"
-FREE_REPO_LINE="deb http://download.proxmox.com/debian/pve stretch pve-no-subscription"
+FREE_REPO_LINE="deb http://download.proxmox.com/debian/pve buster pve-no-subscription"
 
 function pve_patch() {
   echo $FREE_REPO_LINE > $FREE_REPO_LIST

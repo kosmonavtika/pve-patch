a1973f2a3c21c941737f744376c57392075a5790
kosmonavtika <jimmy@vostok.dev>
2019-09-26 19:10:45 +0200

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# pve-patch
+
+Removes subscription dialogs, replaces enterprise repository with non-subscription repository and replaces branding.
+
+## Note
+
+Use at your own risk! Read the script before you run it. 
+
+## Use
+
+1. Connect to node via SSH
+2. Run
+
+```bash
+wget -qO - https://raw.githubusercontent.com/kosmonavtika/pve-patch/master/patch.sh | bash
+```
---
+++ scripts/apply.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+ENTERPRISE_REPO_LIST="/etc/apt/sources.list.d/pve-enterprise.list"
+FREE_REPO_LIST="/etc/apt/sources.list.d/pve-no-subscription.list"
+FREE_REPO_LINE="deb http://download.proxmox.com/debian/pve stretch pve-no-subscription"
+
+function pve_patch() {
+  echo $FREE_REPO_LINE > $FREE_REPO_LIST
+  rm -f $ENTERPRISE_REPO_LIST
+  sed -i.bak "s/data.status !== 'Active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+  cp -f /usr/share/pve-patch/images/* /usr/share/pve-manager/images/
+}
+
+pve_patch

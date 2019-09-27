5dc0751e01083ae81e69733910fc4a7c09503608
kosmonavtika <jimmy@vostok.dev>
2019-09-27 10:30:34 +0200

    update patch installer

--- patch.sh
+++ patch.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-mkdir -p /usr/share/pve-patch/{images,scripts} && \
-echo "- patch `pveversion`..." && \
-echo "- download and copy files..." && \
-wget -qPnc /usr/share/pve-patch/images/ https://github.com/kosmonavtika/pve-patch/raw/master/images/{favicon.ico,logo-128.png,proxmox_logo.png} && \
-wget -qPnc /usr/share/pve-patch/scripts/ https://raw.githubusercontent.com/kosmonavtika/pve-patch/master/scripts/{90pvepatch,apply.sh} && \
-chmod -R a+x /usr/share/pve-patch/scripts && \
-cp -f /usr/share/pve-patch/scripts/90pvepatch /etc/apt/apt.conf.d/90pvepatch && \
-/usr/share/pve-patch/scripts/apply.sh && \
+mkdir -p /usr/share/pve-patch/{images,scripts}
+echo "- patch `pveversion`..."
+echo "- download and copy files..."
+wget -qPnc /usr/share/pve-patch/images/ https://github.com/kosmonavtika/pve-patch/raw/master/images/{favicon.ico,logo-128.png,proxmox_logo.png}
+wget -qPnc /usr/share/pve-patch/scripts/ https://raw.githubusercontent.com/kosmonavtika/pve-patch/master/scripts/{90pvepatch,apply.sh}
+chmod -R a+x /usr/share/pve-patch/scripts
+cp -f /usr/share/pve-patch/scripts/90pvepatch /etc/apt/apt.conf.d/90pvepatch
+/usr/share/pve-patch/scripts/apply.sh
 echo "- done!"

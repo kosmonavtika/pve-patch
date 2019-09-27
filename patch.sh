11eaf4ba8b9b7eb96fecf52045d845bc17889f04
kosmonavtika <jimmy@vostok.dev>
2019-09-27 08:32:33 +0200

    change stdout

--- patch.sh
+++ patch.sh
@@ -7,6 +7,5 @@ wget -qP /usr/share/pve-patch/images/ https://github.com/kosmonavtika/pve-patch/
 wget -qP /usr/share/pve-patch/scripts/ https://raw.githubusercontent.com/kosmonavtika/pve-patch/master/scripts/{90pvepatch,apply.sh} && \
 chmod -R a+x /usr/share/pve-patch/scripts && \
 cp -f /usr/share/pve-patch/scripts/90pvepatch /etc/apt/apt.conf.d/90pvepatch && \
-echo "- apply patch..." && \
 /usr/share/pve-patch/scripts/apply.sh && \
 echo "- done!"

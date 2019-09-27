5e5f4c1546d539b5d6c706def8efc7286a95b1b2
kosmonavtika <jimmy@vostok.dev>
2019-09-27 08:41:51 +0200

    fix stdout

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -5,7 +5,7 @@ FREE_REPO_LIST="/etc/apt/sources.list.d/pve.list"
 FREE_REPO_LINE="deb http://download.proxmox.com/debian/pve `lsb_release -cs` pve-no-subscription"
 
 function pve_patch() {
-  echo "apply patch..."
+  echo "- apply patch..."
   echo $FREE_REPO_LINE > $FREE_REPO_LIST
   [ -f $ENTERPRISE_REPO_LIST ] && mv $ENTERPRISE_REPO_LIST $ENTERPRISE_REPO_LIST~
   sed -i.bak "s/data.status !== 'Active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js

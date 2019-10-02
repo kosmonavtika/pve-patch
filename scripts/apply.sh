bfdc63b7501320a7d6c736b861342d027a954e2d
kosmonavtika <jimmy@vostok.dev>
2019-10-02 22:19:48 +0200

    fix debian version bug

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+DEBIAN_CODENAME=`cat /etc/os-release | grep VERSION_CODENAME | cut -d "=" -f2`
 ENTERPRISE_REPO_LIST="/etc/apt/sources.list.d/pve-enterprise.list"
 FREE_REPO_LIST="/etc/apt/sources.list.d/pve.list"
-FREE_REPO_LINE="deb http://download.proxmox.com/debian/pve `lsb_release -cs` pve-no-subscription"
+FREE_REPO_LINE="deb http://download.proxmox.com/debian/pve $DEBIAN_CODENAME pve-no-subscription"
 
 function pve_patch() {
   echo "- apply patch..."

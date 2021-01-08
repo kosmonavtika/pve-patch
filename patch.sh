7956fd04d92676722304ba43f14b47c14182cd2d
Daniel <git@myrandor.com>
2021-01-08 10:01:38 -0500

    Remove images. Update to script.

--- patch.sh
+++ patch.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir -p /usr/share/pve-patch/{images,scripts}
+mkdir -p /usr/share/pve-patch/scripts
 echo "- patch `pveversion`..."
 echo "- download and copy files..."
-wget -nc -qP /usr/share/pve-patch/images/ https://github.com/kosmonavtika/pve-patch/raw/master/images/{favicon.ico,logo-128.png,proxmox_logo.png}
 rm -f /usr/share/pve-patch/scripts/{90pvepatch,apply.sh}
 wget -qP /usr/share/pve-patch/scripts/ https://raw.githubusercontent.com/kosmonavtika/pve-patch/master/scripts/{90pvepatch,apply.sh}
 chmod -R a+x /usr/share/pve-patch/scripts

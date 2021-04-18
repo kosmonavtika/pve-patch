0be1df7a0d8fd42552df129becf2895d0f1efe34
WhatTheServer <admin@whattheserver.com>
2021-04-18 15:19:08 -0400

    Update patch-test.sh

--- scripts/patch-test.sh
+++ scripts/patch-test.sh
@@ -3,9 +3,9 @@ CONTROLFILE='proxmox-patch-tests-control.txt'
 ORIGINALFILE='proxmox-patch-tests-original.txt'
 TESTFILE='proxmox-patch-tests.txt'
 sed -i "s|if (data.status !== 'Active')|if (false)|g" ${TESTFILE}
-sed -i -z "s/res.*res.*.false/false/g" ${TESTFILE}
-sed -i -z "s/res.*res.*.data.status !== 'Active'/false/g" ${TESTFILE}
-sed -i -z "s/res.*res.*.data.status.toLowerCase() !== 'active'/false/g" ${TESTFILE}
+sed -i 's|res === null \|\| res === undefined \|\| \!res \|\| res|REPLACEME|' ${TESTFILE}
+sed -i -z "s/REPLACEME.*.data.status.toLowerCase() !== 'active'/false/" ${TESTFILE}
+sed -i -z "s/REPLACEME.*data.status !== 'Active'/false/" ${TESTFILE}
 
 echo "Comparing ${TESTFILE} file after replacements with ${ORIGINALFILE}"
 echo '===================================================='

dc182f1e6c500f0c506e00807ff8ec63b6fc0196
WhatTheServer <admin@whattheserver.com>
2021-04-18 13:18:18 -0400

    Add patch test script
    Included is a script to test the patches out locally

---
+++ scripts/patch-test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+CONTROLFILE='proxmox-patch-tests-control.txt'
+ORIGINALFILE='proxmox-patch-tests-original.txt'
+TESTFILE='proxmox-patch-tests.txt'
+sed -i "s|if (data.status !== 'Active')|if (false)|g" ${TESTFILE}
+sed -i -z "s/res.*res.*.false/false/g" ${TESTFILE}
+sed -i -z "s/res.*res.*.data.status !== 'Active'/false/g" ${TESTFILE}
+sed -i -z "s/res.*res.*.data.status.toLowerCase() !== 'active'/false/g" ${TESTFILE}
+
+echo "Comparing ${TESTFILE} file after replacements with ${ORIGINALFILE}"
+echo '===================================================='
+diff -y ${TESTFILE} ${ORIGINALFILE}
+echo '===================================================='
+
+echo "Comparing ${TESTFILE} file after replacements with ${CONTROLFILE}"
+echo '===================================================='
+diff -y ${TESTFILE} ${CONTROLFILE}
+echo '===================================================='
+echo "Run the below to reset the ${TESTFILE} file after replacements with ${ORIGINALFILE}"
+echo '===================================================='
+rm -f "${TESTFILE}" && cp "${ORIGINALFILE}" "${TESTFILE}"
+echo '===================================================='
